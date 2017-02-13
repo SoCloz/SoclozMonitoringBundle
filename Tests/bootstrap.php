8680564b2b7627f4b1ed409ae0d9b61f02435fc8
Jérémy DECOOL <contact@jdecool.fr>
2017-02-13 23:18:36 +0100

    Improve Travis testing (PHP 7.*, Symfony 3.*)

--- Tests/bootstrap.php
+++ Tests/bootstrap.php
@@ -16,3 +16,8 @@ if ((!$loader = includeIfExists(__DIR__.'/../vendor/autoload.php')) && (!$loader
 if (class_exists('Doctrine\Common\Annotations\AnnotationRegistry')) {
     \Doctrine\Common\Annotations\AnnotationRegistry::registerLoader(array($loader, 'loadClass'));
 }
+
+// Keep PHPUnit < 6.0 BC
+if (!class_exists('PHPUnit_Framework_TestCase') && class_exists('PHPUnit\Framework\TestCase')) {
+    class_alias('PHPUnit\Framework\TestCase', 'PHPUnit_Framework_TestCase');
+}
